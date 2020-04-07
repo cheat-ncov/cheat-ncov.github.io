7a96376d90fc298cd57ea5815c1bebe60c31f200
tuan.nguyen10 <tuan.nguyen10@tiki.vn>
2020-04-07 10:57:01 +0700

    build

---
+++ build/precache-manifest.8b11e6f29165ad673d2c87109db603ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bcb2366e9a63ea2f0ac681714f70bcf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "769dfe9b3a56c96d6f18",
+    "url": "/static/css/main.fa8acbf4.chunk.css"
+  },
+  {
+    "revision": "e9a1b9a42fa7e6b01e48",
+    "url": "/static/js/2.bf56f576.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.bf56f576.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "769dfe9b3a56c96d6f18",
+    "url": "/static/js/main.ea604c27.chunk.js"
+  },
+  {
+    "revision": "e39f8272a082e0239ff3",
+    "url": "/static/js/runtime-main.a83114b1.js"
+  }
+]);
--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 export default {
-    API_URL : "http://127.0.0.1:5000/"
+    API_URL : "http://34.71.250.136:5000/"
 }
