92f74c96da92ee98ac1cd42479fc82cb524b45d6
tuan.nguyen10 <tuan.nguyen10@tiki.vn>
2020-04-07 11:16:55 +0700

    Updates

---
+++ precache-manifest.279231d15048d59cb0f1c8fa8556c2f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28f3d6a45656aa324009252fcd503f94",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1a7b22fc8d134ed8b72f",
+    "url": "/static/css/main.fa8acbf4.chunk.css"
+  },
+  {
+    "revision": "730015beba3ace6cb6db",
+    "url": "/static/js/2.5d08751f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.5d08751f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a7b22fc8d134ed8b72f",
+    "url": "/static/js/main.c73a6a72.chunk.js"
+  },
+  {
+    "revision": "b11ba9e9a0713f5b5021",
+    "url": "/static/js/runtime-main.6de58a70.js"
+  }
+]);
